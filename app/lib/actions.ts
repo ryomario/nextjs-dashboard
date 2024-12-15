83725fd4d1931b6621b812cd67d90aadb19f22f1
Mario <ardamomo3@gmail.com>
2024-12-15 17:34:28 +0700

    learn: finish fixed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -98,7 +98,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string, prevState: State) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     return { message: 'Deleted Invoice.' };
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,9 @@
-import { deleteInvoice } from '@/app/lib/actions';
+'use client';
+
+import { deleteInvoice, State } from '@/app/lib/actions';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { useActionState } from 'react';
 
 export function CreateInvoice() {
   return (
@@ -26,9 +29,11 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const initialState: State = { message: null };
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const [state, formAction] = useActionState<State>(deleteInvoiceWithId,initialState);
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={formAction}>
       <button type='submit' className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
